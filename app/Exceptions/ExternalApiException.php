2fb761a3022c465000f3dc316a930b91273ae95f
ericksonferreira <erickson.rinho@hotmail.com>
2025-07-14 04:47:43 -0300

    refactor: melhora a classe ExternalApiException com construtor personalizado

--- app/Exceptions/ExternalApiException.php
+++ app/Exceptions/ExternalApiException.php
@@ -6,5 +6,8 @@ use Exception;
 
 class ExternalApiException extends Exception
 {
-    //
+    public function __construct($message = "Erro na API externa", $code = 500, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
